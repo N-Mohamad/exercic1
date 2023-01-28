851b6883291471e980d1bf92ae27264759a0b767
N-Mohamad <101737216+N-Mohamad@users.noreply.github.com>
2023-01-28 23:07:17 +0100

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,63 @@
+<script>
+let count = 0;
+
+function handleIncrement() {
+    count += 1;
+}
+function handleDecrement() {
+    count -= 1;
+}
+function handleReset() {
+    count = 0;
+}
+</script>
+<br>
+<div id="compteur-clics">
+<div>
+    <div>
+    <input bind:value={count} type="number" id="count" />
+    </div>
+
+    <div>
+    <button id="clics-plus" on:click={handleIncrement}>+</button>
+	<button id="clics-moins" on:click={handleDecrement}>−</button>
+    </div>
+    
+</div>
+<div>
+    <div>
+    <button id="reset" on:click={handleReset}>reset</button>
+    </div>
+</div>
+</div>
+
+<style>
+	#compteur-clics{
+		width: 500px;
+        margin: 0 auto;
+        text-align: center;
+		padding: 20px;
+		background-color: rgb(190, 190, 190);
+	}
+	input{
+		width: 450px;
+		text-align: center;
+	}
+	#clics-plus{
+		width: 225px;
+		height: 55px;
+		color: white;
+		background-color: rgb(72, 129, 190);
+	}
+	#clics-moins{
+		width: 225px;
+		height: 55px;
+		color: white;
+		background-color: rgb(72, 129, 190);
+	}
+	#reset{
+		width: 120px;
+		border-color: red ;
+		color: red;
+	}
+</style>
